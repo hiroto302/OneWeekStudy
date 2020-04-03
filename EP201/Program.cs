6edd579b76fb7bbffc51da8f01b789498c13f3f7
hiroto302 <i.am.a.hiroto@gmail.com>
2020-04-03 20:25:09 +0900

    ExampleProblem201 (2日目例題1)
    コンソールからの整数値入力

---
+++ EP201/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+//コンソールから入力された2つの整数の和を表示するプログラム
+namespace EP201
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a= ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b= ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("a + b = {0}", a + b);
+        }
+    }
+}
