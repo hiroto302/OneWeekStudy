7998625acf7beeab5d7f43d2a56687a5bf2271d3
hiroto302 <i.am.a.hiroto@gmail.com>
2020-04-03 22:26:23 +0900

    EP205
    stringクラスの活用 Equals

---
+++ EP205/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EP205
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Helloと入力して下さい : ");
+            string m = Console.ReadLine();
+            // if( m == "Hello")
+            // stringクラスのEqualsの活用 第２引数にオプションを追加できる
+            // if( m.Equals("Hello"))
+            if( m.Equals("Hello", StringComparison.OrdinalIgnoreCase)) //大文字と小文字を比較しないオプション
+            {
+                Console.WriteLine("{0}が入力されました。", m);
+            }
+            else
+            {
+                Console.WriteLine("Helloと入力してください");
+            }
+        }
+    }
+}
