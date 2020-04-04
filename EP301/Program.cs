f0b4046a11032bea522aa42e3796e9421d040878
hiroto302 <i.am.a.hiroto@gmail.com>
2020-04-04 20:15:46 +0900

    EP301
    繰り返し文
    for, while , do~while

---
+++ EP301/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+//入力された数値の数だけ＊を生成するプログラム 3パターン
+namespace EP301
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("数値の数だけ*を生成します");
+            Console.Write("数値を入力して下さい : ");
+            int n = int.Parse(Console.ReadLine());
+
+            // for文
+            // for( int i = 0; i < n; i++)
+            // {
+            //     Console.Write("* ");
+            // }
+
+            // while文
+            // int i = 0;
+            // while(i < n)
+            // {
+            //     Console.Write("* ");
+            //     i++;
+            // }
+
+            // do~while文
+            if(0 < n)
+            {
+                int i = 0;
+                do
+                {
+                    Console.Write("* ");
+                    i++;
+                }
+                while(i < n);
+            }
+
+            Console.WriteLine();  //生成したあと段落作成
+            Console.WriteLine("{0}個生成しました", n);
+        }
+    }
+}
