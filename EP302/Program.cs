b2963b1c48ac43268e17b96279f76f37ce1c6671
hiroto302 <i.am.a.hiroto@gmail.com>
2020-04-04 21:15:03 +0900

    EP302
    正数の約数を求めるプログラム
    整数と整数の判定も記述

---
+++ EP302/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+// キーボードから正の整数を入力させ、その数値の約数(割り切ることできる数)を全て表示させるプログラム
+namespace EP302
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("正数を入力 : ");
+            var n = float.Parse(Console.ReadLine());
+            // 整数の判定
+            var m = n / Math.Floor(n);
+
+            // 正数の判定
+            if(0 < n　&& m == 1)
+            {
+                for(int i = (int)n; 0 < i; i--)
+                {
+                    // 約数の判定
+                    if(n % i == 0)
+                    {
+                        Console.Write(i + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("適切な数値を入力してください");
+            }
+        }
+    }
+}
