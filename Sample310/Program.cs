4b7139339da73500cd64323109ee5a3c830b099f
hiroto302 <i.am.a.hiroto@gmail.com>
2020-04-05 11:15:45 +0900

    ジャグ配列要素の取り出し方

--- Sample310/Program.cs
+++ Sample310/Program.cs
@@ -15,7 +15,7 @@ namespace Sample310
                 new int[] {2},          //縦1列目(ジャグ配列の中の1番目の配列) m=1, a[m].Length = 1
                 new int[] {3, 4, 5, 6}  //縦2列目(ジャグ配列の中の2番目の配列) m=2, a[m].Length = 4
             };
-            Console.WriteLine(a[0].Length);
+            Console.WriteLine(a[1][0]);  //ジャグ配列の要素の取り出し方
             // 要素の表示
             // a.Length = 3   ジャグ配列 a に格納している配列の数をさす
             for(int m = 0; m < a.Length; m++)
