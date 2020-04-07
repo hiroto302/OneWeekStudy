488cd537e6a89163b08f9a8d494c7064d14b6943
hiroto302 <i.am.a.hiroto@gmail.com>
2020-04-07 16:49:58 +0900

    HashSetクラス実装例

---
+++ Sample604/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+// HashSetクラスの実装例
+// データを重複することなく管理することができる
+namespace Sample604
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> t = new HashSet<int>();
+            t.Add(1);
+            t.Add(3);
+            t.Add(2);
+            t.Add(1);
+            foreach(int i in t)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
