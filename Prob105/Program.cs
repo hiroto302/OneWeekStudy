1b92033dff83cb2c983b43a376e2cecd603cbc3c
hiroto302 <i.am.a.hiroto@gmail.com>
2020-04-08 13:33:28 +0900

    性と名を入力し、フルネームを表示するプログラム

---
+++ Prob105/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+// 性と名を入力し、フルネームを表示するプログラム
+namespace Prob105
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("性を入力 : ");
+            string familyName = Console.ReadLine();
+            Console.Write("名を入力 : ");
+            string firstName = Console.ReadLine();
+            string fullName = familyName + " " + firstName;
+            Console.WriteLine("あなたの名前は、" + familyName + " " + firstName + "です");
+            Console.WriteLine("あなたの名前は、" + fullName + "です");
+        }
+    }
+}
