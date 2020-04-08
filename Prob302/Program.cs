84eff77ddc3b77978920613b67ffe8aa6a515155
hiroto302 <i.am.a.hiroto@gmail.com>
2020-04-08 17:46:14 +0900

    繰り返し文(for , while, do ~while)を利用し、星を5つ表示するプログラム

---
+++ Prob302/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+// 繰り返し文for, while, do~while をそれぞれ利用し、* を5つ表示する
+namespace Prob302
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] n = new string[5];
+            string star = "* ";
+
+            // for文
+            for(int i = 0; i < n.Length; i++)
+            {
+                n[i] = star;
+                Console.Write(n[i]);
+            }
+            Console.WriteLine();
+
+            // while文
+            int j = 0;
+            while( j < n.Length )
+            {
+                n[j] = star;
+                Console.Write(n[j]);
+                j ++;
+            }
+            Console.WriteLine();
+
+            // do~while文
+            int w = 0;
+            do
+            {
+                n[w] = star;
+                Console.Write(n[w]);
+                w++;
+            }
+            while(w < n.Length);
+            Console.WriteLine();
+        }
+    }
+}
