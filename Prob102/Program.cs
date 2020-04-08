10e21756999aa5d363f220eb3e513f61715e2075
hiroto302 <i.am.a.hiroto@gmail.com>
2020-04-08 12:25:34 +0900

    円の半径を0以上の実数値で入力し、その円の面積と円周の長さを表示するプログラム

---
+++ Prob102/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// 円の半径を0以上の実数値で入力し、その円の面積と円周の長さを表示するプログラム
+// 円周率は3.14とすること
+namespace Prob102
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("円の半径を入力 : ");
+            double r = double.Parse(Console.ReadLine());
+            if( r > 0)
+            {
+                Console.WriteLine("面積 = {0}", r * r * 3.14);
+                Console.WriteLine("円周 = {0}", r * 2.0 * 3.14);
+            }
+            else
+            {
+                Console.WriteLine("0以上の値を入力してください");
+            }
+        }
+    }
+}
