6003f13faa56ff4c34803112e95074b6b0162954
hiroto302 <i.am.a.hiroto@gmail.com>
2020-04-08 18:11:41 +0900

    正の整数を入力し、その値から0までカウントダウンを表示するプログラム(for, while)

---
+++ Prob303/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// 正の整数を入力し、その値から0までカウントダウンを表示するプログラム
+namespace Prob303
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("正の整数を入力してください : ");
+            int n = int.Parse(Console.ReadLine());
+            for ( int i = n; 0 <= i; i--)
+            {
+                Console.WriteLine(i);
+            }
+            // while(0 <= n)
+            // {
+            //     Console.WriteLine(n);
+            //     n --;
+            // }
+        }
+    }
+}
