aa4906b5719618494bb68d3bd43133e66c3dff49
hiroto302 <i.am.a.hiroto@gmail.com>
2020-04-08 12:07:16 +0900

    入力した名前を表示するプログラム

---
+++ Prob101/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// 自分の名前を表示するプログラム
+namespace Prob101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("名前を入力 : ");
+            string name = Console.ReadLine();
+            Console.WriteLine("俺の名前は、{0}です", name);
+        }
+    }
+}
