2520abded9772a09d9718fc6b553097e7dc85fee
hiroto302 <i.am.a.hiroto@gmail.com>
2020-04-08 22:26:19 +0900

    月を表す数値を入力させ、それに対応する月の名前を英語で表示するプログラム

---
+++ Prob404/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// 月を表す数値を入力させ、それに対応する月の名前を英語で表示するプログラム
+namespace Prob404
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] months = {"January", "February", "March", "April", "May", "June",
+                                "July", "August","September", "October", "November", "December"};
+            Console.Write("月(1~12)を入力してください : ");
+            int m = int.Parse(Console.ReadLine());
+            if( m >= 1 && m <= 12)
+            {
+                Console.WriteLine("{0}月は、英語で{1}", m, months[m - 1]);
+            }
+            else
+            {
+                Console.WriteLine("適切な値を入力せよ");
+            }
+        }
+    }
+}
