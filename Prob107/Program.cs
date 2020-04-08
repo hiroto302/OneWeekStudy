60a984c333a4c67912426af759c6720c1959fa6c
hiroto302 <i.am.a.hiroto@gmail.com>
2020-04-08 14:43:15 +0900

    * を任意の数だけ横に並べて表示するプログラム

---
+++ Prob107/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// * を任意の数だけ横に並べて表示するプログラム
+namespace Prob107
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("生成したい * の数を入力 : ");
+            int n = int.Parse(Console.ReadLine());
+            string [] stars = new string[n];
+            for ( int i = 0; i < stars.Length; i++)
+            {
+                stars[i] = "* ";
+                Console.Write(stars[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("{0}個生成しました", n);
+        }
+    }
+}
