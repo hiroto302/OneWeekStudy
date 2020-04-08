b5f301bc60b4f52327c9412ae26a057bf4ed0c41
hiroto302 <i.am.a.hiroto@gmail.com>
2020-04-08 14:26:27 +0900

    四則演算

---
+++ Prob106/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+// 四則演算の正しい結果を表示できるように修正する
+namespace Prob106
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+            Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
+            Console.WriteLine("{0} * {1} = {2}", a, b, a * b);
+            Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
+            Console.WriteLine("{0} % {1} = {2}", a, b, a % b);
+        }
+    }
+}
