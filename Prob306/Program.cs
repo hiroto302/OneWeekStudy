ab2d41791766b46e92bad903ac645ca97c55fd4b
hiroto302 <i.am.a.hiroto@gmail.com>
2020-04-08 20:56:06 +0900

    乱数を1~10を生成し、配列の要素を６つに代入する。その配列の要素を表示するプログラム

---
+++ Prob306/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// 入力した文字を返し、何も入力さずにEnterを押した時終了するプログラム
+namespace Prob306
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.Write("文字を入力 : ");
+                string text = Console.ReadLine();
+                if( text == "")
+                {
+                    break;
+                }
+                {
+                    Console.WriteLine(text);
+                }
+            }
+        }
+    }
+}
---
+++ Prob401/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+// 乱数を1~10を生成し、配列の要素を６つに代入する。その配列の要素を表示するプログラム。
+namespace Prob401
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rundom = new Random();
+            int[] num = new int[6];
+            for(int i = 0; i < num.Length; i++)
+            {
+                int rundomNum = rundom.Next(1, 11);
+                num[i] = rundomNum;
+                Console.WriteLine("num[{0}] = {1}", i, num[i]);
+            }
+        }
+    }
+}
