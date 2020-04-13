fa37dfac2651f6ba45f30dcc651506a6eabe1c79
hiroto302 <i.am.a.hiroto@gmail.com>
2020-04-13 21:02:44 +0900

    入力された数値の三角関数の計算結果を表示するプログラム(Mathクラスの利用)

---
+++ EP502/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+// 入力された数値の三角関数の計算結果を表示するプログラム
+// Mathクラスの利用 数学の処理の静的メンバがまとめられている
+namespace EP502
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("角度を入力 : ");
+            double angle = double.Parse(Console.ReadLine());
+            // 角度を弧度法に変換
+            double rad = Math.PI * angle / 180.0;
+            // 入力した角度のsin, cos, tan の表示
+            Console.WriteLine("sin{0} = {1}", angle, Math.Sin(rad));
+            Console.WriteLine("cos{0} = {1}", angle, Math.Cos(rad));
+            Console.WriteLine("tan{0} = {1}", angle, Math.Tan(rad));
+        }
+    }
+}
