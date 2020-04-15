06e53d1ef408ec9b70bcc523cae8e74de446b7ac
hiroto302 <i.am.a.hiroto@gmail.com>
2020-04-15 18:36:24 +0900

    orループを用いて、1~100までの乱数を5個発生させ、その中の最大値、最小値を求めるプログラム (Listクラスのメソッド活用 etc)

---
+++ Prob305/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// forループを用いて、1~100までの乱数を5個発生させ、その中の最大値、最小値を求めるプログラム
+namespace Prob305
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<int> numbers = new List<int>();
+            for(int i = 0; i < 5; i++)
+            {
+                int n = random.Next(1, 101);
+                numbers.Add(n);
+            }
+            foreach(int i in numbers)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+            Console.WriteLine("最大値 : {0}", numbers.Max());
+            Console.WriteLine("最小値 : {0}", numbers.Min());
+        }
+    }
+}
