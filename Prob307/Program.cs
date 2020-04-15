c2cd25d145104f41b103c249e6916510a8f2ee8a
hiroto302 <i.am.a.hiroto@gmail.com>
2020-04-15 18:56:18 +0900

    forの二重ループを用いて */ を交互に表示するプログラム

---
+++ Prob307/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+// forの二重ループを用いて */ を交互に表示するプログラム
+namespace Prob307
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 8; i > 0; i--)
+            {
+                for(int j = 4; j > 0; j--)
+                {
+                    if(i % 2 == 0)
+                    {
+                        Console.Write("*/");
+                    }
+                    else
+                    {
+                        Console.Write("/*");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
