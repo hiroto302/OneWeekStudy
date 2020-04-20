5a3173cda9688b714dc3ff1a9637616229417f9d
hiroto302 <i.am.a.hiroto@gmail.com>
2020-04-20 19:47:53 +0900

    EP406に2つのコンストラクタを追加したプログラム

---
+++ EP409/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+// EP406に2つのコンストラクタを追加したプログラム
+// コンストラクタ対応した実行処理に変更
+namespace EP409
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bingo bingo = new Bingo();
+            while(true)
+            {
+                try
+                {
+                        Console.Write("ビンゴの作成サイズを入力 : ");
+                        int size = int.Parse(Console.ReadLine());
+                        if(size > 0)
+                        {
+                            Bingo bingo = new Bingo(size);
+                            Console.WriteLine();
+                            // bingo.Init(size);
+                            bingo.Show();
+                            break;
+                        }
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("正数を入力してください");
+                }
+            }
+        }
+    }
+}
