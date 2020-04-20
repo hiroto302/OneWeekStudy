2efb48710d4023b033de9439551a9300cef0c53f
hiroto302 <i.am.a.hiroto@gmail.com>
2020-04-20 20:34:05 +0900

    EP409(ビンゴ)におけるArrayクラスのメソッドを利用したプログラム( Array.Clear(array, index, length))_

---
+++ EP503/Bingo.cs
@@ -0,0 +1,76 @@
+using System;
+
+// Clearメソッドの簡略化 Arrayクラスのメソッド使用
+namespace EP503
+{
+  class Bingo
+  {
+    // ビンゴのサイズ
+    private int size = 0;
+    private int[,] bingo;
+
+    // 引数なしのコンストラクタ 5*5のビンゴカード作成
+    public Bingo() : this(5)
+    {
+      // Init(5);
+    }
+    // 引数ありのコンストラクタ 引数にsizeを受け取り、対応したビンゴカードを作成
+    public Bingo(int size)
+    {
+      Init(size);
+    }
+    //入力に応じたビンゴカード作成するメソッド
+    public void Init(int size)
+    {
+      this.size = size;
+      bingo = new int[size, size];
+      // ビンゴカードの初期化
+      Clear();
+      // サイズに対応した値をランダムに代入
+      Shuffle();
+    }
+
+    // ビンゴカードの表示
+    public void Show()
+    {
+      for(int i = 0; i < bingo.GetLength(0); i++)
+      {
+        for(int j = 0; j < bingo.GetLength(1); j++)
+        {
+          Console.Write("{0, 2:d}|", bingo[i,j]);
+        }
+        Console.WriteLine();
+        for(int j = 0; j < bingo.GetLength(1); j++)
+        {
+          Console.Write("---");
+        }
+        Console.WriteLine();
+      }
+    }
+
+    // bingoの値を全て0にするメソッド
+    private void Clear()
+    {
+      Array.Clear(bingo, 0, bingo.Length);
+      // クリアする配列
+      // 削除する要素の範囲開始のインデックス
+      // 削除する要素の数
+    }
+    // 数値をランダムに配置するメソッド
+    private void Shuffle()
+    {
+      int num = 1;
+      Random random = new Random();
+      while( num <= size*size)
+      {
+        int m = random.Next(size);
+        int n = random.Next(size);
+        if(bingo[m, n] == 0)
+        {
+          bingo[m, n] = num;
+          num++;
+        }
+      }
+    }
+  }
+}
---
+++ EP503/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+// EP409(ビンゴ)におけるArrayクラスのメソッドを利用したプログラム
+namespace EP503
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bingo bingo = new Bingo();
+            while(true)
+            {
+                try
+                {
+                        Console.Write("ビンゴの作成サイズを入力 : ");
+                        int size = int.Parse(Console.ReadLine());
+                        if(size > 0)
+                        {
+                            Bingo bingo = new Bingo(size);
+                            Console.WriteLine();
+                            // bingo.Init(size);
+                            bingo.Show();
+                            break;
+                        }
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("正数を入力してください");
+                }
+            }
+        }
+    }
+}
